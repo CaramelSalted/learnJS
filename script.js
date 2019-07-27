5bb8557f6a624c96d97f0124d75f51ea55b38f78
Kate <ktrnwb@yandex.ru>
2019-07-27 13:53:29 +0300

    Результаты третьего урока

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let money = prompt("Ваш бюджет?");
-let name = prompt("Название вашего магазина?");
+let money,
+	name,
+	time,
+	price = 100;
+
+function astart (){
+	
+	do{
+		money = prompt("Ваш бюджет?");
+	} while (isNaN(money) || money == "" || money == null);
+
+	name = prompt("Название вашего магазина?").toUpperCase();
+	time = 18
+}
 
 var mainList = {
 	budget: money,
@@ -7,10 +19,47 @@ var mainList = {
 	shopGoods: [],
 	employers: {},
 	open: false,
+	discount: true
 }
 
-for (let i = 0; i < 4; i++){
-	mainList.shopGoods[i] = prompt("Что вы продаете?");
+function chooseGoods(n){
+	for (let i = 0; i < n; i++){
+	let a = prompt("Какой тип товаров будем продавать?");
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a!= ''){
+			mainList.shopGoods[i] = a;
+		} else {
+			i = i - 1;
+		}
+	}
 }
 
-alert("Ваш бюджет на день " + (mainList.budget / 30));
+function workTime (time) {
+	if (time < 0) {
+		console.log("Такого не может быть");
+	} else if (time > 8 && time <20){
+			console.log("Время работать!");
+		} else if (time < 24){
+				console.log("Уже очень поздно");
+			} else {
+				console.log("В сутках только 24 часа")
+				}
+}
+
+const isDiscount = () => mainList.discount? 0.8 * price : price;
+
+
+function dailyBudget (){
+	alert("Ваш бюджет на день " + (mainList.budget / 30));
+}
+
+function hireEmployers (n) {
+	for (let i = 0; i < n; i++){
+	let b = prompt("Введите имя сотрудника:");
+		if ((typeof(b)) === 'string' && (typeof(b)) != null && b!= ''){
+			mainList.employers.key = i + 1;
+			mainList.employers.name = a;
+		} else {
+			i = i - 1;
+		}
+	}
+}
